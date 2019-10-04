5a85d49f27810ee0085a9329d68c390a19fe23c9
Ben Smith <bsmith@apl.washington.edu>
2019-10-04 14:39:27 -0600

    ATL06_tiles.py: Added a 'rel' argument and forced consistent directory structure

--- ATL06_tiles.py
+++ ATL06_tiles.py
@@ -16,7 +16,7 @@ from PointDatabase.mapData import mapData
 from PointDatabase.ATL06_filters import segDifferenceFilter
 from LSsurf.unique_by_rows import unique_by_rows
 import sys
-from PointDatabase.check_ATL06_blacklist import check_rgt_cycle_blacklist
+
 from glob import glob
 from PointDatabase.geo_index import geo_index, index_list_for_files
 
@@ -24,7 +24,7 @@ import os
 import re 
 
 def make_tile(args):
-    #xy0, SRS_proj4, tile_spacing, pad, bin_W, GI_file, out_dir, field_dict, seg_diff_scale=None, blockmedian_scale=None, blacklist=None):
+
     xy0=args['xy0']
     SRS_proj4=args['SRS_proj4']
     tile_spacing=args['tile_spacing']
@@ -43,10 +43,12 @@ def make_tile(args):
     for group in field_dict:
         for ds in field_dict[group]:
             list_of_fields.append(ds)
-  
+
     gI=geo_index().from_file(GI_file, read_file=False)
     out_file=out_dir+('/E%d_N%d.h5' % (xy0[0]/1.e3, xy0[1]/1.e3))
-    print("working on "+out_file)
+    if os.path.isfile(out_file):
+        os.remove(out_file)
+        
     D=gI.query_xy((xy0[0]+dxb, xy0[1]+dyb), fields=field_dict)
     if D is None:
         return
@@ -108,6 +110,9 @@ def read_tile(xy0, tile_dir,  W=None):
         g=re.compile('N(.*)_E(.*).h5').search(os.path.basename(tile_file))
         x0=np.float(g.group(1))*1000
         y0=np.float(g.group(2))*1000
+    else:
+        x0=None
+        y0=None
     with h5py.File(tile_file,'r') as h5f:
         # figure out what fields to read:
         # find a group in h5f that contains fields:
@@ -150,7 +155,7 @@ def reconstruct_tracks(D, x0=None, y0=None, W=None):
         D0.append(this_D)   
     return D0
 
-def make_queue(tile_root, GI_file, queue_file, cycle=1, hemisphere=-1, tile_spacing=1.e5):
+def make_queue(tile_root, GI_file, queue_file, cycle=1, hemisphere=-1, tile_spacing=1.e5, rel='001'):
 
     """
     make a queue of commands to generate the tiles for ATL06
@@ -187,7 +192,7 @@ def make_queue(tile_root, GI_file, queue_file, cycle=1, hemisphere=-1, tile_spac
     this_python_file=os.path.abspath(sys.argv[0])
     with open(queue_file,'w') as qf:
         for xy in xyTile:
-            qf.write(f"python3 {this_python_file} {tile_root} -G {GI_file} -H {hemisphere} --xy {xy[0]} {xy[1]} -c {cycle}" + "\n")
+            qf.write(f"python3 {this_python_file} {tile_root} -G {GI_file} -H {hemisphere} --xy {xy[0]} {xy[1]} -c {cycle} --rel {rel}" + "\n")
   
 def index_tiles(tile_dir_root, cycle, hemisphere):
      """
@@ -242,13 +247,17 @@ def test_plot(gi_file):
     for D in D_list:
         plt.plot(D.x, D.y,'.')
     plt.show()
+    plt.close()
+    print('Use control-C to close figure.')
+    #plt.savefig('fig name')
 
 
 def main():
     import argparse
-    parser=argparse.ArgumentParser('Save ATL06 data into large but manageable blocks')
+    parser=argparse.ArgumentParser('Save ATL06 data into large but manageable blocks, called tiles.')
     parser.add_argument('tile_root', type=str, help='root directory for tiles.')
-    parser.add_argument('-H', '--Hemisphere', type=int, default=-1, help='Hemisphere.  +1 for arctic, -1 for Antarctic')
+    parser.add_argument('--rel',type=str,default='001',help='Release number, including all three digits.')
+    parser.add_argument('-H', '--Hemisphere', type=int, default=-1, help='Hemisphere.  +1 for Arctic, -1 for Antarctic')
     parser.add_argument('--xy', type=float, nargs=2, help='bin center location, m: x,y')
     parser.add_argument('-c','--cycle', type=int, default=None, help='Cycle number')
     parser.add_argument('-W','--W_bin', type=float, default=1.e4, help='Bin spacing, m')
@@ -258,38 +267,39 @@ def main():
     parser.add_argument('--index_of_cycles', action='store_true',\
                         help='index the indices for the cycle tiles.  Run this after --index_of_tiles, requires tile root')
     parser.add_argument('-q', '--queue_file', default=None, help='make a queue of commands to make tiles in a cycle.  Requires tile_root, --cycle, --xy, --ATL06_geoindex')
-    parser.add_argument('-G','--Geoindex', type=str, help='ATL06 geoindex location.')
+    parser.add_argument('-G','--Geoindex', type=str, help='ATL06 geoindex location.  Can include a %s that will be replaced by the cycle number.')
     parser.add_argument('--test_plot', action='store_true', help="make a test plot.  Need to secify a geoindex file (-G)")
     args=parser.parse_args()
+    print('args',args)
 
     if args.test_plot:
         test_plot(args.Geoindex)
         sys.exit(0)
 
-    #make_queue('/home/ben/temp/GL_tile_queue.txt', hemisphere=1)
-    #index_tiles('/Volumes/ice2/ben/scf/GL_06/tiles/001/', 1)
     if args.index_of_cycles:
-        index_cycle_indices(args.tile_root, args.Hemisphere)
+        index_cycle_indices(args.tile_root+'/tiles/'+args.rel, args.Hemisphere)
         sys.exit(0)
     if args.index_of_tiles:
-        index_tiles(args.tile_root, args.cycle, args.Hemisphere)
+        index_tiles(args.tile_root+'/tiles/'+args.rel, args.cycle, args.Hemisphere)
         sys.exit(0)
         
     try:
-        GI_file=args.Geoindex % args.cycle
+        GI_file=args.Geoindex % args.cycle  
     except TypeError:
         GI_file=args.Geoindex
         
     if args.queue_file is not None:
         #make_queue(tile_root, GI_file, queue_file, hemisphere=-1, tile_spacing=1.e5
-        make_queue(args.tile_root, GI_file, args.queue_file, hemisphere=args.Hemisphere, cycle=args.cycle, tile_spacing=args.Tile_spacing)
+        make_queue(args.tile_root, GI_file, args.queue_file, hemisphere=args.Hemisphere, cycle=args.cycle, rel=args.rel, tile_spacing=args.Tile_spacing)
         sys.exit(0)
         
     pad=0
     blockmedian_scale=None
     #Skip seg_diff_Scale (August 27: changed from 5 to None)
     seg_diff_scale=None
-    out_dir=args.tile_root+('/cycle_%02d' % args.cycle)
+    out_dir=args.tile_root+'/tiles/'+args.rel+('/cycle_%02d' % args.cycle)
+    print('out dir',out_dir)
+
     if not os.path.isdir(out_dir):
         os.mkdir(out_dir)
         
